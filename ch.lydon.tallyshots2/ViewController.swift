cad7134fc7ce5b57a7c4160bd0beb556f79b2feb
Lydon Chandra <lydonchandra@icloud.com>
2019-03-13 00:04:49 +0800

    display hit-miss on watch, hardcoded

---
+++ ch.lydon.tallyshots2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ch.lydon.tallyshots2
+//
+//  Created by Don on 12/3/19.
+//  Copyright © 2019 Don. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
