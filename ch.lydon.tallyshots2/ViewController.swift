92f306940ea579bb8362a79c15924f835b550241
Lydon Chandra <lydonchandra@icloud.com>
2019-03-17 19:05:51 +0800

    using CoreData

--- ch.lydon.tallyshots2/ViewController.swift
+++ ch.lydon.tallyshots2/ViewController.swift
@@ -7,39 +7,103 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "TallyShot")
+        
+        do {
+            tallyShots = try managedContext.fetch(fetchRequest)
+        }
+        catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate;
-        //let entity = NSEntityDescription.entity
+
+        title = "Shots Tallies"
         
-        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
     
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        if sender.title(for: .normal) == "X" {
-            sender.setTitle("long titleeeeeeeee", for: .normal)
-        }
-        else {
-            sender.setTitle("X", for: .normal)
-        }
-    }
+    var tallyShots: [NSManagedObject] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func addTallyShot(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            [unowned self] action in
+            
+            guard let textField = alert.textFields?.first,
+                let nameToSave = textField.text else {
+                    return
+            }
 
-    override var shouldAutorotate: Bool {
-        return true
+            self.save(name: nameToSave)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
     }
     
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .all
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "TallyShot", in: managedContext)!
+        
+        let tallyShot = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        tallyShot.setValue(name, forKeyPath: "name")
+        
+        do {
+            try managedContext.save()
+            tallyShots.append(tallyShot)
+        }
+        catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
     }
-//    
-//    func navigationControllerSupportedInterfaceOrientations(_ navigationController: UINavigationController) -> UIInterfaceOrientationMask {
-//        return .all
-//    }
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tallyShots.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tallyShot = tallyShots[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = tallyShot.value(forKeyPath: "name") as? String
+        
+        return cell
+    }
+}
 
